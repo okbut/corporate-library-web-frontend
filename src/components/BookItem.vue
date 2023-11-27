2a7b03266a990c82d62a6db922f1941f3a0b56af
tyange <usun16@gmail.com>
2023-11-28 06:57:51 +0900

    add: BookList & BookItem

---
+++ src/components/BookItem.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  thumbnail?: string
+  title: string
+}>()
+</script>
+<template>
+  <div>
+    <span>{{ title }}</span>
+  </div>
+</template>
--- src/components/layouts/TheLayout.vue
+++ src/components/layouts/TheLayout.vue
@@ -5,7 +5,7 @@ import TheHeader from '../ui/TheHeader.vue'
 <template>
   <div class="flex flex-col items-center w-full min-h-screen">
     <TheHeader />
-    <main class="flex-1 w-2/5">
+    <main class="flex flex-col items-center justify-center flex-1 w-2/5">
       <slot />
     </main>
   </div>
