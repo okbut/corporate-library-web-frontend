cdef935faa5afe84690db93daa3b3d60864623bb
tyange <usun16@gmail.com>
2023-12-05 20:01:34 +0900

    style: daisy-ui로 꾸미기

--- src/components/ui/TheHeader.vue
+++ src/components/ui/TheHeader.vue
@@ -2,40 +2,45 @@
   <div class="navbar bg-base-100">
     <div class="navbar-start">
       <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-5 h-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h7"
-            />
-          </svg>
-        </div>
-        <ul
-          tabindex="0"
-          class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li><a>관리자</a></li>
-          <li><a>도서 검색</a></li>
-        </ul>
+        <details class="dropdown">
+          <summary class="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="w-5 h-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </summary>
+          <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+            <li><a>도서 검색</a></li>
+            <li><a>관리자</a></li>
+          </ul>
+        </details>
       </div>
     </div>
     <div class="navbar-center">
       <a class="text-xl btn btn-ghost">사내도서관</a>
     </div>
     <div class="navbar-end">
-      <button class="btn btn-ghost btn-circle">
-        <div class="indicator">
-          <span class="material-symbols-outlined"> account_circle </span>
-        </div>
-      </button>
+      <div class="dropdown dropdown-end">
+        <details class="dropdown">
+          <summary class="btn btn-ghost btn-circle">
+            <span class="material-symbols-outlined"> account_circle </span>
+          </summary>
+          <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+            <li><a>로그인</a></li>
+            <li><a>계정 생성</a></li>
+          </ul>
+        </details>
+      </div>
     </div>
   </div>
 </template>
