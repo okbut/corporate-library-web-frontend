dde5abf784bfa2234c0a30d719fc39ad55341e52
tyange <usun16@gmail.com>
2023-12-14 06:46:23 +0900

    add: baseUrl

--- src/api/books.ts
+++ src/api/books.ts
@@ -1,11 +1,13 @@
+const baseUrl = import.meta.env.MODE !== 'production' ? '/' : ''
+
 export const BooksAPI = {
   list: async (pageNum: number) => {
-    const response = await fetch(`/api/books?page=${String(pageNum)}&size=30`)
+    const response = await fetch(`${baseUrl}api/books?page=${String(pageNum)}&size=30`)
 
     return response
   },
   detail: async (code: string) => {
-    const response = await fetch(`/api/books/${code}`)
+    const response = await fetch(`${baseUrl}api/books/${code}`)
 
     return response
   }
