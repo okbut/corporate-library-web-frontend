70c052a8c27b427be09636abb54341186e35b1df
tyange <usun16@gmail.com>
2023-12-14 22:01:45 +0900

    add: 도서 생성 form 마크업

---
+++ src/components/BookForm.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import BookFormInput from './BookFormInput.vue'
+import BookFormTextarea from './BookFormTextarea.vue'
+</script>
+
+<template>
+  <form class="flex flex-col">
+    <book-form-input label="제목" name="title"></book-form-input>
+    <book-form-textarea label="책 소개" name="description"></book-form-textarea>
+    <book-form-input label="저자" name="author"></book-form-input>
+    <book-form-input label="출판사" name="publisher"></book-form-input>
+    <book-form-input label="발행 연도" name="publisher"></book-form-input>
+  </form>
+</template>
